ca14faa6b7f5d60a5a27e9877912544382ff5850
HarleyMorales <harleymoraa6@gmail.com>
2024-03-07 15:40:37 -0500

    Initialize app state variables and cached element references. Fixed HTML some more

---
+++ script.js
@@ -0,0 +1,11 @@
+// Section 1: TODOs
+// TODO: Register submissions from the user on the form.
+// TODO: Determine the value of the data submitted and add it to a JavaScript array called N/A not given
+// TODO: Call the render function to update the table with the new tasks.
+
+let tasks = [];
+const tasksForm = document.getElementById("taskForm");
+const taskTable = document.getElementById("taskTable");
+
+console.log(tasksForm);
+console.log(taskTable);
