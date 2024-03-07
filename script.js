97db6513aa8ab0470613e1fe2d84fc9660c39b20
HarleyMorales <harleymoraa6@gmail.com>
2024-03-07 16:18:36 -0500

    Implement form submission handling

--- script.js
+++ script.js
@@ -1,11 +1,34 @@
-// Section 1: TODOs
-// TODO: Register submissions from the user on the form.
-// TODO: Determine the value of the data submitted and add it to a JavaScript array called N/A not given
-// TODO: Call the render function to update the table with the new tasks.
-
 let tasks = [];
 const tasksForm = document.getElementById("taskForm");
 const taskTable = document.getElementById("taskTable");
 
-console.log(tasksForm);
-console.log(taskTable);
+// Function to handle form submissions
+function handleSubmission(event) {
+    event.preventDefault();
+    // TODO: Get form input values
+    const taskName = document.getElementById('taskName').value
+    const taskDescription = document.getElementById('taskDescription').value
+    const taskDeadline = document.getElementById('taskDeadline').value
+    // TODO: Validate input fields
+    if(taskName.trim === "" || taskDeadline === ""){
+        alert('Task name and deadline are required!');
+        return;
+    };
+    // TODO: Update the tasks array
+    tasks.push({name: taskName, description: taskDescription, deadline: taskDeadline});
+    render();
+}
+
+// Function to render tasks in the table
+function render() {
+    // TODO: Use array methods to create a new table row of data for each item in the arr
+}
+
+// Function to initialize the table
+function init() {
+    taskTable.innerHTML = '';
+    tasks = [];
+    render(); 
+}
+
+document.getElementById("taskForm").addEventListener('submit', handleSubmission);
